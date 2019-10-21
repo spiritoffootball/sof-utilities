55143171823edcaf3aedbfa0728350c30d34ec93
Christian Wach <needle@haystack.co.uk>
2019-10-21 23:25:56 +0100

    Transfer [team] shortcode from "spiritoffootball" theme

--- includes/sof-shortcodes.php
+++ includes/sof-shortcodes.php
@@ -35,15 +35,101 @@ class Spirit_Of_Football_Shortcodes {
 	public function register_hooks() {
 
 		// Register shortcodes.
-		//add_shortcode( 'quote', array( $this, 'quote_shortcode' ) );
+		add_shortcode( 'team', array( $this, 'team_render' ) );
 
 	}
 
 
 
-
 	// #########################################################################
 
+
+
+	/**
+	 * Add the team to a page via a shortcode.
+	 *
+	 * @since 0.3 Moved here from the "spiritoffootball" theme.
+	 *
+	 * @param array $attr The saved shortcode attributes.
+	 * @param str $content The enclosed content of the shortcode.
+	 * @return str $team The HTML markup for the shortcode.
+	 */
+	public function team_render( $attr, $content = null ) {
+
+		// Init return.
+		$team = '';
+
+		// Init defaults.
+		$defaults = array(
+			'include' => '7, 3, 2, 8, 4, 5', // Default set of team members.
+		);
+
+		// Parse attributes.
+		$shortcode_atts = shortcode_atts( $defaults, $attr, 'team' );
+
+		// Build include users array from attribute.
+		$include_users = explode( ',', $shortcode_atts['include'] );
+
+		// Sanitise array items.
+		array_walk( $include_users, function( &$item ) {
+			$item = absint( trim( $item ) );
+		});
+
+		// Define args (users in order)
+		$args = array(
+			'include' => $include_users,
+			'orderby' => 'include',
+		);
+
+		// Get the users by ID.
+		$users = get_users( $args );
+
+		// Did we get any?
+		if ( count( $users ) > 0 ) {
+
+			// Open div.
+			$team .= '<div id="sof_team">' . "\n";
+
+			// Loop through sorted users.
+			foreach( $users AS $user ) {
+
+				// Get data.
+				$user_data = get_userdata( $user->ID );
+
+				// Open internal wrapper.
+				$team .= '<div class="sof_team_member clearfix">' . "\n";
+
+				// Show display name.
+				$team .= '<h2>' . esc_html( $user->display_name ) . '</h2>' . "\n";
+
+				// Add gravatar.
+				$team .= '<div class="author_avatar">' . get_avatar( $user_data->user_email, $size='200' ) . '</div>' . "\n";
+
+				// Show description.
+				$team .= '<p>' . esc_html( nl2br( $user_data->description ) ) . '</p>' . "\n";
+
+				// Show link to profile if we're a super admin.
+				if ( is_multisite() AND is_super_admin() ) {
+					$url = admin_url( 'user-edit.php?user_id=' . $user->ID );
+					$team .= '<p><a class="post-edit-link" href="' . $url . '">' .
+								__( 'Edit this profile', 'spiritoffootball' ) .
+							 '</a></p>' . "\n";
+				}
+
+				// Close sof_team_member.
+				$team .= '</div>' . "\n";
+
+			}
+
+			// Close sof_team.
+			$team .= '</div>' . "\n";
+
+		}
+
+		// --<
+		return $team;
+
+	}
 
 
 
