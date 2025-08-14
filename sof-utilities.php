52d24233de1ae3b21392288531956f941baf9a3d
Christian Wach <needle@haystack.co.uk>
2025-08-14 10:47:29 +0100

    Bump version to 1.0.0a

--- sof-utilities.php
+++ sof-utilities.php
@@ -5,7 +5,7 @@
  * Plugin Name: SOF Utilities
  * Description: Network-wide utilities for the SOF sites.
  * Plugin URI:  https://github.com/spiritoffootball/sof-utilities
- * Version:     0.4.1
+ * Version:     1.0.0a
  * Author:      Christian Wach
  * Author URI:  https://haystack.co.uk
  * Text Domain: sof-utilities
@@ -18,7 +18,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Set our version here.
-define( 'SOF_UTILITIES_VERSION', '0.4.1' );
+define( 'SOF_UTILITIES_VERSION', '1.0.0a' );
 
 // Store reference to this file.
 if ( ! defined( 'SOF_UTILITIES_FILE' ) ) {
